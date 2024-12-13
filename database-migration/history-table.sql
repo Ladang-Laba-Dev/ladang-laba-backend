718ac050654788e3305c9974d8ee53c89e6f6ee1
Muhammad Sirojuddin <nidu672@gmail.com>
2024-12-13 14:31:09 +0700

    add history table to save the prediction

---
+++ database-migration/history-table.sql
@@ -0,0 +1,15 @@
+#
+# TABLE STRUCTURE FOR: history
+#
+
+DROP TABLE IF EXISTS `history`;
+
+CREATE TABLE `history` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `crop_name` varchar(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `month` int(11) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
